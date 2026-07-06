5233edf573ba0119ce11f744a1596775e2edc46b
Gunashreejr10 <gunashreevijay29@gmail.com>
2026-07-06 11:23:10 +0530

    Implement Adapter Pattern for PayPal Payment Gateway

--- Module- 1 -- Design Patterns/Adapter Pattern — Payment Gateway.java
+++ Module- 1 -- Design Patterns/Adapter Pattern — Payment Gateway.java
@@ -1,1 +1,26 @@
 
+
+interface PaymentProcessor {
+    void processPayment(int amount);
+}
+
+class PayPalGateway { // incompatible interface
+    void makePayment(int amount) {
+        System.out.println("Paid " + amount + " using PayPal");
+    }
+}
+
+class PayPalAdapter implements PaymentProcessor {
+    private PayPalGateway payPal = new PayPalGateway();
+
+    public void processPayment(int amount) {
+        payPal.makePayment(amount);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        PaymentProcessor processor = new PayPalAdapter();
+        processor.processPayment(500);
+    }
+}
